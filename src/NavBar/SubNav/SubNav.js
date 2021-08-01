4d79f526f0accb82cbfcb4d880cb5d98dab3a300
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-07-31 18:44:47 -0700

    added subnav

--- src/NavBar/SubNav/SubNav.js
+++ src/NavBar/SubNav/SubNav.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { SubNavItem } from './SubNavItem/SubNavItem';
-// timport styles from './SubNavItem.module.css';
+import styles from './SubNav.module.css';
 
 export function SubNav() {
     return (
-        <div>
-            <div>
-                <SubNavItem label="Restaurant" icon="fa-utensils"/>
-            </div>
-            <div>
-                <button className="button">
-                    <span className="icon"><i className="fas fa-pen"/></span>
-                    <span>Write a Review</span>
-                </button>
+        <div className={styles.container}>
+            <div className={styles['sub-nav']}>
+                <div>
+                    <SubNavItem label="Restaurant" icon="fa-utensils"/>
+                </div>
+                <div>
+                    <button className={`button ${styles['subnav-button']} ${styles['omit-right-border']}`}>
+                        <span className="icon"><i className="fas fa-pen"/></span>
+                        <span>Write a Review</span>
+                    </button>
+                </div>
             </div>
         </div>
     );
