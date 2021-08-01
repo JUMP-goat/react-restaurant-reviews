5c319a9dfa5eb86736eef17748355c3ead412429
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-08-01 13:36:37 -0700

    configuring api endpoints

--- src/hooks/yelp-api/api.js
+++ src/hooks/yelp-api/api.js
@@ -1,13 +1,14 @@
-import {API_BASE_URL, BEARER_TOKEN} from './config';
+// import {API_BASE_URL, BEARER_TOKEN} from './config';
+import {API_BASE_URL} from './config';
 import queryString from 'query-string'
 
 export function get(path, queryParams) {
     const query = queryString.stringify(queryParams);
     return fetch(`${API_BASE_URL}${path}?${query}`, {
         headers: {
-            Authorization: `Bearer ${BEARER_TOKEN}`,
+            // Authorization: `Bearer ${BEARER_TOKEN}`,
             Origin: 'localhost',
-            withCredentials: true,
+            // withCredentials: true,
         }
     });
 }
--- src/hooks/yelp-api/config.js
+++ src/hooks/yelp-api/config.js
@@ -1,8 +1,5 @@
-export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jsonplaceholder.typicode.com/todos/1';
+// export const API_BASE_URL = 'http://ec2-52-90-183-165.compute-1.amazonaws.com:8080/api/users';
+export const API_BASE_URL = 'http://localhost:8080/api/users';
 
 // FIXME: remove this token
-// NOTE: as long as this token is saved in the frontend, the application is not production ready.
-// The Baerer token must be used on a server whose inner workings are not exposed to the outside world
-// For simplicity and the sake of this tutorial, I'll just leave it here like this
-// But just bear in mind: NEVER EVER save a bearer token inside the frontend code
 export const BEARER_TOKEN = '';
