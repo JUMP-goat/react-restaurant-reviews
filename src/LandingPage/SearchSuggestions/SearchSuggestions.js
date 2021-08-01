a8a370bc3df5ebf624234790a0e2319422faa48a
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-08-01 01:55:03 -0700

    added hooks, rating, search, etc...

--- src/LandingPage/SearchSuggestions/SearchSuggestions.js
+++ src/LandingPage/SearchSuggestions/SearchSuggestions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './SearchSuggestions.module.css'
+import styles from './SearchSuggestions.module.css';
 
 export function SearchSuggestions() {
     return (
@@ -7,4 +7,4 @@ export function SearchSuggestions() {
             <span className="icon is-small"><i className="fas fa-utensils"></i></span><span className={styles.suggestion}>Restaurants</span>
         </div>
     );
-};
+}
--- src/LandingPage/TopNav/TopNav.js
+++ src/LandingPage/TopNav/TopNav.js
@@ -1,11 +1,12 @@
-import React from "react";
-import styles from "./TopNav.module.css";
+import React from 'react';
+import styles from './TopNav.module.css';
 
 export function TopNav() {
     return (
         <div className={styles['top-nav']}>
             <div className={styles.left}>
-                <span>Write a review</span>
+                <span>Write a Review</span>
+                <span>Events</span>
             </div>
             <div className={styles.right}>
                 <span>Login</span>
